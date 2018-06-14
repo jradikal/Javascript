989c4656ffa8a63881d327b0bfaa761efd2c5411
Jared <jaredjayz@hotmail.com>
2018-06-14 12:37:08 +0100

    Added stalker exercise and age calc

---
+++ webdev bc/Stalker Exercise/1 Stalker exercise/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+var fullName = firstName + " " + lastName
+
+console.log("Your name is, " + fullName);
+console.log("Your age is " + userAge + " years old");
---
+++ webdev bc/Stalker Exercise/2 Age calc/script.js
@@ -0,0 +1,5 @@
+var years = prompt("How old are you?");
+
+var howManyDays = years * 365.25;       //.25 accounts for the leap year
+
+alert("You are roughly " + howManyDays + " days old!");
