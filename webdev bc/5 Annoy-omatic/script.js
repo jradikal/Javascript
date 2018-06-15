09eb920f9002708e6dd5bcbb371cbe21ec4fd7ae
Jared <jaredjayz@hotmail.com>
2018-06-15 11:55:12 +0100

    Added annoy o matic

---
+++ webdev bc/5 Annoy-omatic/script.js
@@ -0,0 +1,16 @@
+var yet = prompt("Are we there yet?");
+
+while(yet !== "yes" && yet !== "yeah") {
+    var yet = prompt("Are we there yet");
+}
+
+alert("Yay, we finally made it!");
+
+
+// var yet = prompt("are we there yet?");
+
+// while(yet.indexOf("yes") === -1) {
+// 	var yet = prompt("are we there yet?");
+// }
+
+// alert("YAY, WE MADE IT!!!");
